b95bb8c80e803a4251f7190bd2875640850ac362
Sebastian Gebhardt <sebastiangebhardt@gmail.com>
2023-03-02 11:44:05 +0000

    service file + switch to POST for node red compability

--- webdpm.py
+++ webdpm.py
@@ -23,12 +23,12 @@ def read_power():
 def read_output():
     return {"Output": dpm.output()} 
 
-@app.patch("/output")
-def patch_output(set: str):
-    if set in ['1', 'on']:
-        return {"action": dpm.output(dpm.P_ON)} # output 1/on -- turn the output on"
+@app.post("/output")
+def post_output(value: str):
+    if value in ['1', 'on']:
+        return {"on": dpm.output(dpm.P_ON)} # output 1/on -- turn the output on"
     else:
-        return {"action": dpm.output(dpm.P_OFF)} # output 0/off/"else" -- turn the output off
+        return {"off": dpm.output(dpm.P_OFF)} # output 0/off/"else" -- turn the output off
     
 @app.get("/voltage")
 def read_voltage(q: Union[str, None] = None):
@@ -39,8 +39,8 @@ def read_voltage(q: Union[str, None] = None):
     else:
         return {"voltage": dpm.voltage()} # read the actual delivered voltage
 
-@app.patch("/voltage")
-def patch_voltage(value: float): # set the voltage target
+@app.post("/voltage")
+def post_voltage(value: float): # set the voltage target
     return {"voltage": dpm.setting("v",value)}
 
 @app.get("/current")
@@ -52,18 +52,18 @@ def read_current(q: Union[str, None] = None):
     else:
         return {"current": dpm.current()} #  read the actual deliviered current
     
-@app.patch("/current")
-def patch_voltage(value: float): # set the current target
+@app.post("/current")
+def post_voltage(value: float): # set the current target
     return {"current": dpm.setting("c",value)}
 
 @app.get("/const")
 def read_const():
     return {"const": dpm.const()} #  read the actual const setting (voltage/const)
     
-@app.patch("/const_voltage")
-def patch_const_voltage(value: float): # set constant voltage delivery
+@app.post("/const_voltage")
+def post_const_voltage(value: float): # set constant voltage delivery
     return {"const_voltage": dpm.const(dpm.P_VOLTAGE)}
 
-@app.patch("/const_current")
-def patch_const_current(value: float): # set constant current delivery
+@app.post("/const_current")
+def post_const_current(value: float): # set constant current delivery
     return {"const_current": dpm.const(dpm.P_CURRENT)}
